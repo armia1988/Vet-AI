fb217f7bf824b8b1c370d106c68fdd8e636a6684
armia1988 <armia2999@gmail.com>
2026-09-04 15:03:10 +0200

    Launch Vet AI V5

--- lib/main_v2.dart
+++ lib/main_v2.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'config/supabase_config.dart';
-import 'v4_app.dart';
+import 'v5_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,5 +16,5 @@ Future<void> main() async {
     publishableKey: SupabaseConfig.publishableKey,
   );
 
-  runApp(const VetAIAppV4());
+  runApp(const VetAIAppV5());
 }
