a1278b1c3b5bf254e5900d36cebdcf511f2e193d
armia1988 <armia2999@gmail.com>
2026-09-08 23:06:05 +0200

    Grant service role access for WhatsApp delivery

---
+++ supabase/migrations/0102_whatsapp_service_role_grants.sql
@@ -0,0 +1,5 @@
+grant select, insert, update on public.whatsapp_alert_preferences to service_role;
+grant select, insert, update on public.whatsapp_deliveries to service_role;
+grant select on public.farms to service_role;
+grant select on public.profiles to service_role;
+grant select on public.alerts to service_role;
