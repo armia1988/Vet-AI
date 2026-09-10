be143f32e089623aaa998f7fe9fec3a9e2e0195d
armia1988 <armia2999@gmail.com>
2026-09-10 18:48:12 +0200

    Chain V76 support overlay compile fix

--- tools/patch_v63e_profile_metadata_nullsafe.py
+++ tools/patch_v63e_profile_metadata_nullsafe.py
@@ -117,3 +117,4 @@ runpy.run_path('tools/patch_v73_voice_player_live_wave_chat_cues.py', run_name='
 runpy.run_path('tools/patch_v73a_value_listenable_import.py', run_name='__main__')
 runpy.run_path('tools/patch_v74_safari_audio_keyboard_live_wave.py', run_name='__main__')
 runpy.run_path('tools/patch_v75_support_realtime_voice_precision.py', run_name='__main__')
+runpy.run_path('tools/patch_v76_support_overlay_compile_and_navigation.py', run_name='__main__')
